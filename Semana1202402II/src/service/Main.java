4579db50c6891027f36be3eff7dfebbee80257bc
Sergio <Sergio@LAPTOP-CAAB5F6K>
2024-05-21 20:48:36 -0600

    Semana 1

--- Semana1202402II/src/service/Main.java
+++ Semana1202402II/src/service/Main.java
@@ -9,29 +9,29 @@ import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		String nombre = "Sergio";
 		String apellido1 = "Campos";
 		String apellido2 = "Rodriguez";
-		
-		System.out.println("El nombre del profesor es " + nombre + " " + apellido1 + " " + apellido2);
+
+		System.out.println("El nombre del profesor es: " + nombre + " " + apellido1 + " " + apellido2);
 		System.out.printf("El nombre del profesor es: %1s %2s %3s", nombre, apellido1, apellido2);
-		
+
 		Date fechaHoy = new Date();
-		
+
 		System.out.printf("\nLa fecha de hoy es: %1$td-%1$tm-%1$ty", fechaHoy);
-		
+
 		Scanner scan = new Scanner(System.in);
-		
+
 		System.out.println("\nDigame su edad");
 		int edad = scan.nextInt();
-		
+		System.out.println("\nLa Edad digitada es: " + edad);
+
+
 		String estadoCivil = JOptionPane.showInputDialog("Estado Civil");
-		
+
 		JOptionPane.showMessageDialog(null, "Su estado civil es: " + estadoCivil);
-		
-		
-		System.out.println("Digite opcion \n1 Dividir numeros\n2 Separar letras\n3\n4");
+
+		System.out.println("Digite una opcion \n1 Dividir numeros\n2 Separar letras\n3 Operaciones\n4 Saludos");
 		int op = scan.nextInt();
 		switch (op) {
 		case 1:
@@ -51,15 +51,40 @@ public class Main {
 			do {
 				System.out.println("\nIndique una palabra para deletrar");
 				palabra = scan.next();
-				if(!palabra.equals("FIN")) {
-					for(int i=0; i<palabra.length();i++) {
-						System.out.print(palabra.charAt(i) + " ") ;
+				if (!palabra.equals("FIN")) {
+					for (int i = 0; i < palabra.length(); i++) {
+						System.out.print(palabra.charAt(i) + " ");
 					}
 				}
-				
-			}while(!palabra.equals("FIN"));
+
+			} while (!palabra.equals("FIN"));
+			break;
+		case 3:
+			System.out.println("Digite un numero");
+			double num1 = scan.nextDouble();
+			System.out.println("Digite un numero");
+			double num2 = scan.nextDouble();
+			System.out.printf("La suma de %1.2f + %2.2f = %3.2f\n", num1, num2, (num1 + num2));
+			System.out.printf("La resta de %1.2f - %2.2f = %3.2f\n", num1, num2, (num1 - num2));
+			System.out.printf("La division de %1.2f / %2.2f = %3.2f\n", num1, num2, (num1 / num2));
+			System.out.printf("La la multiplicacion de %1.2f * %2.2f = %3.2f\n", num1, num2, (num1 * num2));
+			System.out.printf("La suma de los resultados es %1.2f\n",
+					((num1 + num2) + (num1 - num2) + (num1 / num2) + (num1 * num2)));
+			break;
+		case 4:
+			boolean seguir = true;
+			String name = "";
+			do {
+				System.out.println("Digame su nombre para saludarlo");
+				name = scan.next();
+				JOptionPane.showMessageDialog(null, "Hola!! " + name, "Saludo", JOptionPane.WARNING_MESSAGE);
+				System.out.println("Desea continuar?? True/False");
+				seguir = scan.nextBoolean();
+			} while (seguir);
+			JOptionPane.showMessageDialog(null, "Adios!! ", "Despedida", JOptionPane.INFORMATION_MESSAGE);
 			break;
 		}
+		scan.close();
 	}
 
 }
